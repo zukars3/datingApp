f21ca2198baebfa533fa11d7ea0a1307f79262be
zukars3 <59807969+zukars3@users.noreply.github.com>
2020-06-04 18:46:36 +0300

    Added method not allowed custom message

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="text-center empty">
+                <h1>You are not supposed to be here :)</h1>
+        </div>
+    </div>
+@endsection
+<style>
+    .empty {
+        margin-top: 10%;
+        text-align: center;
+    }
+</style>
